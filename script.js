377ba56043a66e3dd76372fdd35cbafef68de18f
R0L3eX9 <razvan9rolex@outlook.com>
2020-08-14 00:39:53 +0300

    When the page is loaded a password is already generated

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const generateBtn = document.getElementById('generate');
 const copyBtn = document.getElementById('copy');
 const password = document.getElementById('generated-password');
 
-generateBtn.addEventListener('click', () => {
-    let newpassword = "";
+let newpassword = "";
+
+function generatePassword() {
+    newpassword = ""
     let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*';
     for (let i = 0; i < 16; i++) {
         newpassword += letters.charAt(Math.floor(Math.random() * letters.length));
     }
     password.innerHTML = newpassword;
+    return newpassword;
+}
+
+password.innerHTML = generatePassword()
+
+generateBtn.addEventListener('click', () => {
+    generatePassword();
 })
 
 console.log(copyBtn)
