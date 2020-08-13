075df7f83e0b096af42fe6db8f53510549d69d25
R0L3eX9 <razvan9rolex@outlook.com>
2020-08-14 00:35:03 +0300

    Added javascript, the ability to generate a password and copy it

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello World!')
+const generateBtn = document.getElementById('generate');
+const copyBtn = document.getElementById('copy');
+const password = document.getElementById('generated-password');
+
+generateBtn.addEventListener('click', () => {
+    let newpassword = "";
+    let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*';
+    for (let i = 0; i < 16; i++) {
+        newpassword += letters.charAt(Math.floor(Math.random() * letters.length));
+    }
+    password.innerHTML = newpassword;
+})
+
+console.log(copyBtn)
+
+copyBtn.addEventListener('click', () => {
+    window.navigator.clipboard.writeText(password.innerHTML);
+})
